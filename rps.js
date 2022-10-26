4a479c5bcc80499f3ba286e7ed01e9e1bdc33f4e
bilel <bilel.moudache0412@gmail.com>
2022-10-26 13:09:24 +0200

    Create function playRound
    This function are the rules of the game
    Make choice of the winner by to parameter computerSelection, playerSelection
    And return the result
    
    Also make the variable playerSelection case-insensitive with toLocalLowerCase function
    If you ant more information check the code i make some commentary.

--- rps.js
+++ rps.js
@@ -3,14 +3,29 @@
 randomly rock, paper or scissors */
 function getComputerChoice(){           //Declare and named the function
     const randomNumb = Math.random();   //Create variable randomNumb and assigned with Math.random number between 0 and 1
-    if(randomNumb >= 0.66){             //I choice to divide 1 by 3 and make 3 interval 
-        return "scissors";              //Each interval match with scissor, rock or paper 
+    if(randomNumb >= 0.66){             //I choice to divide 1 by 3 and make 3 interval this line is 1 to 0.66  
+        return "scissors";              //Each interval match with scissor, rock or paper and this interval match with scissors and return it
     }
-    else if (randomNumb <= 0.33){
-        return "rock";                  //And finally return the string 
+    else if (randomNumb <= 0.33){       //If randomNumb < or = to 0.33 then look line 10 
+        return "rock";                  //The function return the string rock 
     }
-    else{
-        return "paper";
+    else{                               //As the function Math.random return number between 1 and 0 the last interval is between 0.66 and 0.33 no other choice possible that is why i make just else.
+        return "paper";                 //The function return the string paper
     }
 }
-console.log(getComputerChoice())
+
+const playerSelection = prompt("rock paper or scissors ?").toLocaleLowerCase(); //Declare and assign the result of prompt function (witch is pop for user can enter value)to playerSelection
+
+const computerSelection = getComputerChoice();                                  //Declare and assign the result of getComputerChoice function to computerSelection
+
+function playRound(playerSelection, computerSelection) {                        //Create playRound function and have to parameter that is the precedent variable
+    if(playerSelection === computerSelection){                                  //If playerSelection its strictly equal (value and type) then
+        return "It's tie !"                                                     //Function return it's tie
+    }
+    else if((playerSelection === "rock" && computerSelection === "scissors")||(playerSelection === "paper" && computerSelection === "rock")||(playerSelection === "scissors" && computerSelection ==="paper")){
+        return "You win "+playerSelection+" beat "+computerSelection+".";       //If the player select rock and the computer scissors or things like that, you know the game. && = and , and || = or, then i hope you understand  
+    }                                                                           //Function return "You win rock beat scissors."
+    else{
+        return "You loose "+computerSelection+" beat "+playerSelection+".";     //And then if other if not match its mean the player loose 
+    }
+  }
